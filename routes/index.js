b84ad25dc71aac84211f7c88c3f4dd3ca8077bd6
Aiden McLeish <aidenmcleish@u.boisestate.edu>
2026-04-16 21:57:51 +0000

    Added basic comment functionality

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,13 @@ router.get('/about',function(req,res,next){
 
 router.get('/comments',function(req,res,next){
   try {
-  res.render('comments',{title: 'Comments'});
+    req.db.query('SELECT * FROM todos;', (err, results) => {
+      if (err) {
+        console.error('Error fetching todos:', err);
+        return res.status(500).send('Error fetching todos');
+      }
+    res.render('comments',{title: 'Comments', todos: results});
+    });
   } catch (error) {
     console.error('Error fetching items:', error);
     res.status(500).send('Error fetching items');
@@ -44,39 +50,21 @@ router.get('/menu',function(req,res,next){
   }
 });
 
-router.post('/create', function (req, res, next) {
-    const { task } = req.body;
+router.post('/comment', function (req, res, next) {
+    const { comment } = req.body;
     try {
-      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => {
+      req.db.query('INSERT INTO todos (task) VALUES (?);', [comment], (err, results) => {
         if (err) {
-          console.error('Error adding todo:', err);
-          return res.status(500).send('Error adding todo');
+          console.error('Error posting comment:', err);
+          return res.status(500).send('Error posting comment');
         }
         console.log('Todo added successfully:', results);
         // Redirect to the home page after adding
-        res.redirect('/');
+        res.redirect('/comments');
       });
     } catch (error) {
-      console.error('Error adding todo:', error);
-      res.status(500).send('Error adding todo');
-    }
-});
-
-router.post('/delete', function (req, res, next) {
-    const { id } = req.body;
-    try {
-      req.db.query('DELETE FROM todos WHERE id = ?;', [id], (err, results) => {
-        if (err) {
-          console.error('Error deleting todo:', err);
-          return res.status(500).send('Error deleting todo');
-        }
-        console.log('Todo deleted successfully:', results);
-        // Redirect to the home page after deletion
-        res.redirect('/');
-    });
-    }catch (error) {
-        console.error('Error deleting todo:', error);
-        res.status(500).send('Error deleting todo:');
+      console.error('Error posting comment:', error);
+      res.status(500).send('Error posting comment');
     }
 });
 
